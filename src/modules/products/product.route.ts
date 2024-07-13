3585dafd346ee6a2a1dae0d56e125eacff17846f
pantho0 <panthomashreky6@gmail.com>
2024-07-13 21:54:24 +0600

    product add and get by ids api added

--- src/modules/products/product.route.ts
+++ src/modules/products/product.route.ts
@@ -1,10 +1,35 @@
 import express, { Request, Response } from "express"
+import { Product } from "./product.model";
 
 const router = express.Router();
 
 
-router.post('/' , (req:Request,res:Response)=>{
-    res.send("hi")
+router.get("/",async(req:Request, res:Response)=>{
+    const products = await Product.find()
+    res.json({
+        success:true,
+        message : "Products retrived successfully",
+        data : products
+    })
+})
+
+router.post('/add-product' , async(req:Request,res:Response)=>{
+    const result = await Product.create(req.body)
+    res.json({
+        success : true,
+        message : "Your product added successfully",
+        data : result
+    })
+})
+
+router.get("/:id", async(req:Request,res:Response)=>{
+    const id = req.params.id;
+    const singleProduct = await Product.findById(id)
+    res.json({
+        success : true,
+        message : "Product retrived successfully",
+        data : singleProduct
+    })
 })
 
 

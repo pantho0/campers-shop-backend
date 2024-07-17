9cbd85ad98e647268320d94e2eacffb54e3065c1
pantho0 <panthomashreky6@gmail.com>
2024-07-17 13:15:14 +0600

    product delete api added

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
-import express, { Request, Response } from 'express'
-import { ProductRoutes } from './modules/products/product.route'
-const app = express()
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { ProductRoutes } from "./modules/products/product.route";
+const app = express();
 
 //parsers
-app.use(express.json())
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
+app.use("/api/v1/products", ProductRoutes);
 
-app.use('/api/v1/products', ProductRoutes)
-
-app.get('/', (req:Request, res:Response) => {
-  res.send('Camp Server is up')
-})
-
-
+app.get("/", (req: Request, res: Response) => {
+  res.send("Camp Server is up");
+});
 
 export default app;
--- src/modules/products/product.route.ts
+++ src/modules/products/product.route.ts
@@ -1,36 +1,44 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
 import { Product } from "./product.model";
 
 const router = express.Router();
 
+router.get("/", async (req: Request, res: Response) => {
+  const products = await Product.find();
+  res.json({
+    success: true,
+    message: "Products retrived successfully",
+    data: products,
+  });
+});
 
-router.get("/",async(req:Request, res:Response)=>{
-    const products = await Product.find()
-    res.json({
-        success:true,
-        message : "Products retrived successfully",
-        data : products
-    })
-})
+router.post("/add-product", async (req: Request, res: Response) => {
+  const result = await Product.create(req.body);
+  res.json({
+    success: true,
+    message: "Your product added successfully",
+    data: result,
+  });
+});
 
-router.post('/add-product' , async(req:Request,res:Response)=>{
-    const result = await Product.create(req.body)
-    res.json({
-        success : true,
-        message : "Your product added successfully",
-        data : result
-    })
-})
+router.get("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const singleProduct = await Product.findById(id);
+  res.json({
+    success: true,
+    message: "Product retrived successfully",
+    data: singleProduct,
+  });
+});
 
-router.get("/:id", async(req:Request,res:Response)=>{
-    const id = req.params.id;
-    const singleProduct = await Product.findById(id)
-    res.json({
-        success : true,
-        message : "Product retrived successfully",
-        data : singleProduct
-    })
-})
-
+router.delete("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const singleProduct = await Product.deleteOne({ _id: id });
+  res.json({
+    success: true,
+    message: "Product deleted successfully",
+    data: singleProduct,
+  });
+});
 
 export const ProductRoutes = router;
